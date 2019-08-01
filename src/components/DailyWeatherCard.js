909f0ce0258205819c3870fee6ba27bb01fed4e4
github username <saramschwarz@gmail.com>
2019-08-01 16:35:54 -0400

    moment and card format

--- src/components/DailyWeatherCard.js
+++ src/components/DailyWeatherCard.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-const DailyWeatherCard = (props) => {
+const moment = require('moment');
+
+const DailyWeatherCard = ({ reading }) => {
+  let newDate = new Date();
+  const dayOfWeek = reading.dt * 1000
+  newDate.setTime(dayOfWeek)
 
   return (
-    <div>
-      <h1>Daily weather card</h1>
+    <div className="col-sm-2">
+      <div className="card">
+        <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+        <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+        <h3>{Math.round(reading.main.temp)} °F</h3>
+        <div className="card-body">
+          <p className="card-text">{reading.weather[0].description}</p>
+        </div>
+      </div>
     </div>
   )
 }
